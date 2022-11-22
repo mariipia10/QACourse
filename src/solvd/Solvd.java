7c578a8a415d11f1771cf005254acd47499773c0
mpheller <mp.heller.10@gmail.com>
2022-11-22 10:10:00 -0300

    test

---
+++ src/solvd/Solvd.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ * 
+ */
+package solvd;
+
+/**
+ *
+ * @author piahe
+ */
+public class Solvd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
